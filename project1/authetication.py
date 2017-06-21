6c484e01ec844961dc595b571efdf4eb69545780
李峰 <lifeng03@corp.netease.com>
2017-06-21 10:57:12 +0800

    添加项目文件以及测试文件

---
+++ project1/authetication.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+def login(username, password):
+    try:
+        user_file = open('/etc/users.txt')
+        user_buf = user_file.read()
+        users = [line.split("|") for line in user_buf.split("\n")]
+        if [username, password] in users:
+            return True
+        else:
+            return False
+    except Exception, exc:
+        print "I can't autheticate you."
+        return False
+
+
+def logout():
+    print "这一行不会被测试用例覆盖到"
+    print "这一行也不会被测试用例覆盖到"
